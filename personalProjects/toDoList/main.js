b9483960247f5c6c6ba7cab6832faa0b2dd43dfa
Ever Lizarraga <rolank.utn@gmail.com>
2024-11-25 18:48:16 -0300

    Fixed view Quick Panel

--- personalProjects/toDoList/main.js
+++ personalProjects/toDoList/main.js
@@ -1,5 +1,38 @@
 import * as appToDo from "./modules/contorlerApp.js";
 
 appToDo.init();
+// window.addEventListener('resize', () => {
+//   console.log('Windows: Resize !!!');
+// });
 
 
+// const headerPanel = document.querySelector('.quick-input-panel__header h3');
+// const floatingActions = document.querySelector('.floating-actions');
+// // floatingActions.style.
+// // floatingActions.style.marginBottom = '20px';
+
+// // Esta API es más moderna y precisa, pero no está soportada en todos los navegadores
+// if (window.visualViewport) {
+//   // alert('Soporta el navegador a .visualViewport');
+//   window.addEventListener('resize', () => {
+//   // window.visualViewport.addEventListener('resize', () => {
+//       const alturaViewport = window.visualViewport.height;
+//       const alturaVentana = window.innerHeight;
+      
+//       // Si hay una diferencia significativa, probablemente el teclado está visible
+//       const tecladoVisible = alturaVentana - alturaViewport > 120;
+//       const diferencia = alturaVentana - alturaViewport;
+      
+//       if (tecladoVisible) {
+//         console.log('Teclado visible');
+//         headerPanel.textContent = `[HW:${alturaVentana}][HV:${alturaViewport}]`;
+//         floatingActions.style.marginBottom = `${diferencia}px`;
+//         // Ajustar UI
+//       } else {
+//         console.log('Teclado oculto');
+//         headerPanel.textContent = `[HW:${alturaVentana}][HV:${alturaViewport}]`;
+//         floatingActions.style.marginBottom = `${diferencia}px`;
+//           // Restaurar UI
+//       }
+//   });
+// }
--- personalProjects/toDoList/modules/ui/quickInputPanel.js
+++ personalProjects/toDoList/modules/ui/quickInputPanel.js
@@ -5,20 +5,34 @@ const form = document.querySelector('.quick-input-panel__form');
 const textArea = document.querySelector('.quick-input-panel__info-todo');
 // const btnSend = document.querySelector('.quick-input-panel__send');
 
+const bodyMain = document.querySelector('.body__main');
+
 let fnCallback = () => {console.log('__Send')};
+let visibleTextArea = false;
 
 function init() {
+
   btnAdd.classList.remove('disable');
   form.addEventListener('submit', fnQuickInputPanel);
   btnCancel.addEventListener('click', () => {
     controlQuickInputPanel.textArea = '';
     quickInputPanel.classList.add('disable');
     btnAdd.classList.remove('disable');
+    visibleTextArea = false;
+    reestoreMarginBottomToViewQuickPanel();
   });
+
   btnAdd.addEventListener('click', () => {
     btnAdd.classList.add('disable');
     quickInputPanel.classList.remove('disable');
     textArea.focus();
+    viewQuickInputPanel();
+    visibleTextArea = true;
+    addMarginBottomToViewQuickPanel();
+  });
+  
+  textArea.addEventListener('focus', () => {
+    viewQuickInputPanel();
   });
 }
 
@@ -26,6 +40,8 @@ function fnQuickInputPanel(event) {
   event.preventDefault();
   fnCallback();
   console.log('Submit interseptado');
+  visibleTextArea = false;
+  reestoreMarginBottomToViewQuickPanel();
 }
 
 function hidePanel() {
@@ -42,6 +58,46 @@ const controlQuickInputPanel = {
 
   hidePanel() {hidePanel()},
 }
+
+function simpleScroll(child) {
+  child.scrollIntoView({ behavior: 'smooth', block: 'start' });
+}
+
+function viewQuickInputPanel() {
+  simpleScroll(quickInputPanel);
+  // timerOfCallback(100, simpleScroll(quickInputPanel));
+  let value = 20;
+  while(value > 0) {
+    value -= 1;
+    timerOfCallback(50+value*40, () => {simpleScroll(quickInputPanel)});
+
+  }
+  timerOfCallback(900, () => {simpleScroll(quickInputPanel)});
+}
+
+function addMarginBottomToViewQuickPanel() {
+  if(visibleTextArea) {
+    if(bodyMain.clientHeight > window.innerHeight) {
+      const cardsList = document.querySelector('.cards-list');
+      const heightPanel = quickInputPanel.clientHeight;
+      cardsList.style.paddingBottom = `${heightPanel - 84+10}px`;
+    }
+  }
+}
+
+function reestoreMarginBottomToViewQuickPanel() {
+  const cardsList = document.querySelector('.cards-list');
+  cardsList.style.paddingBottom = `0px`;
+}
+
+function timerOfCallback(timeout, callback) {
+  setTimeout(() => {
+    callback();
+    // console.log(`Timeout: ${timeout}`);
+  }, timeout);
+  console.log('setTimeout OK [%d]', timeout);
+}
+
 
 const API = controlQuickInputPanel;
 
