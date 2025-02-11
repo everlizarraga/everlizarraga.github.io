677e2fac5a1be3202ccbbf789ed1e2a049cdee03
Ever Lizarraga <rolank.utn@gmail.com>
2025-02-11 19:45:59 -0300

    Corregido autoHideSidebar por resizing

--- personalProjects/theMovieDB/modules/menu.js
+++ personalProjects/theMovieDB/modules/menu.js
@@ -11,19 +11,41 @@ function init() {
     menuBar.classList.toggle('menu-effect-in');
   });
 
-  window.addEventListener('resize', resizeMenu);
+  window.addEventListener('resize', () => {
+    resizeMenu();
+    autoHideSidebar();
+  });
 
   resizeMenu();
 }
 
+function currentViewport() {
+  return document.body.clientWidth;
+}
 
 function resizeMenu() {
   console.log('Resizing Menu ...');
-  const currentViewport = document.body.clientWidth;
-  if(currentViewport < 960)  {
+  if(currentViewport() < 960)  {
     menuBar.style.height = `${document.body.clientHeight - header.clientHeight}px`;
   } else {
     menuBar.style.height = 'auto';
+  }
+}
+
+function autoHideSidebar() {
+  if(menuBar.classList.contains('menu-effect-in')
+  && currentViewport() >= 960) {
+    activeSidebar(false);
+  }
+}
+
+function activeSidebar(stateBoole) {
+  if(stateBoole) {
+    menuBar.classList.add('menu-effect-in');
+    btnMenu.classList.add('btn-change');
+  } else {
+    menuBar.classList.remove('menu-effect-in');
+    btnMenu.classList.remove('btn-change');
   }
 }
 
