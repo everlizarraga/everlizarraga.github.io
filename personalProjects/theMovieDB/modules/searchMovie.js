d17977aa7680c63b5bb5914096f0afc090eeac00
Ever Lizarraga <rolank.utn@gmail.com>
2025-02-26 03:33:33 -0300

    Search funcional - header y menu

---
+++ personalProjects/theMovieDB/modules/searchMovie.js
@@ -0,0 +1,31 @@
+import * as apiTMDB from "./apiTMDB.js";
+import * as helpers from "../utils/helpers.js";
+
+const formSearchHeader = document.querySelector('.search-header');
+const searchHeader = formSearchHeader.querySelector('.search__input');
+
+const formSearchMenu = document.querySelector('.search-menu');
+const searchMenu = formSearchMenu.querySelector('.search__input');
+
+function init() {
+  formSearchHeader.addEventListener('submit', function(event) {
+    event.preventDefault();
+    console.log('formSearchHeader');
+    executeSearch(searchHeader);
+  });
+  formSearchMenu.addEventListener('submit', function(event) {
+    event.preventDefault();
+    console.log('formSearchMenu');
+    executeSearch(searchMenu);
+  });
+}
+
+function executeSearch(inputText) {
+  const movieSearch = inputText.value;
+  if(movieSearch.length == 0) {alert('Input vacio');}
+  inputText.value = '';
+  location.hash = `#search=${helpers.API.replaceCharacters(movieSearch, " ", "_")}`;
+}
+
+
+export { init };
